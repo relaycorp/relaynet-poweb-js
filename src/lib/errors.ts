0139b214bdade1fe9b830eaadbbee87f9191efe2
Gus Narea <gnarea@users.noreply.github.com>
2022-06-14 10:37:47 +0100

    fix

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -3,7 +3,7 @@
 import { VError } from 'verror';
 
 export abstract class PoWebError extends VError {
-  get name(): string {
+  override get name(): string {
     return this.constructor.name;
   }
 }
